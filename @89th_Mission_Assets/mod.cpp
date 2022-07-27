83b2e3dc233640bbafc5e888f1089041558960ca
Flipoholic <83806159+FlipperG@users.noreply.github.com>
2022-07-26 23:58:27 -0500

    Added mod.cpp

---
+++ @89th_Mission_Assets/mod.cpp
@@ -0,0 +1,8 @@
+name="89th Mission Assets"
+picture="";
+logo="";
+logoOver="";
+actionName="GitHub"
+action="https://github.com/FlipperG/89th-Mission-Assets"
+overview="Mod Team will end you.";
+logoSmall="";

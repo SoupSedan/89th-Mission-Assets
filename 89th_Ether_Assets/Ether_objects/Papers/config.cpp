687e89b3d1b0b96bea1c958c10b512380de5a6e0
Flipoholic <83806159+FlipperG@users.noreply.github.com>
2022-08-03 22:10:33 -0500

    Ether papers config

---
+++ 89th_Ether_Assets/Ether_objects/Papers/config.cpp
@@ -0,0 +1,20 @@
+Class CfgPatches
+{
+Class Ether_papers
+{
+units[]=
+{
+
+};
+requiredVersion=0.1
+requiredAddons[]=
+{
+"89th_Assets_Core",
+"A3_structures_f"
+};
+};
+};
+Class CfgVehicles
+{
+
+};

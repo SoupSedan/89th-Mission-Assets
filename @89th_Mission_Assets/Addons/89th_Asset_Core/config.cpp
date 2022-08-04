dd73d4d8ad9f1a0622ef9dc380a2850ba9cf7787
Flipoholic <83806159+FlipperG@users.noreply.github.com>
2022-08-03 22:39:14 -0500

    Asset core config

---
+++ @89th_Mission_Assets/Addons/89th_Asset_Core/config.cpp
@@ -0,0 +1,33 @@
+class CfgPatches 
+ { 
+         class 89th_Asset_Core
+         { 
+                 author="89th JTF Mod Team"; 
+                 name="89th Asset"; 
+                 units[]={}; 
+                 weapons[]={}; 
+                 magazines[]={}; 
+                 ammo[]={}; 
+                 requiredVersion=0.1; 
+                 version=0.2; 
+                 requiredAddons[]={}; 
+         }; 
+ }; 
+ class CfgEditorCategories 
+ { 
+         class 89th_EdCat_Asset
+         { 
+                 displayName="[89th JTF] Assets"; 
+         }; 
+ }; 
+ class CfgEditorSubcategories 
+ { 
+         class 89th_EdSubCat_Asset_Enviro 
+         { 
+                 displayName="Enviroment"; 
+         }; 
+         class 89th_EdSubCat_Asset_null 
+         { 
+                 displayName="null"; 
+         }; 
+ };

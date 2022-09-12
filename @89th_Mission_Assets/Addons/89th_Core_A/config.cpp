178f853c2ee5ac2e40e20118c0f541b54509b98d
Flipoholic <kreepythegamer@gmail.com>
2022-09-12 17:36:11 -0500

    Initial

--- @89th_Mission_Assets/Addons/89th_Core_A/config.cpp
+++ @89th_Mission_Assets/Addons/89th_Core_A/config.cpp
@@ -13,9 +13,13 @@ class CfgPatches
 };
 class CfgEditorCategories
 {
-	class 89th_EdCat_Ether
+	class 89th_EdCat_JTF
 	{
-		displayName="[89th] Ether";
+		displayName="[89th] Misc.";
+	};
+	class 89th_EdCat_Nov_2022
+	{
+		displayName="[89th] Buck";
 	};
 };
 class CfgEditorSubcategories
@@ -23,5 +27,30 @@ class CfgEditorSubcategories
 	class 89th_EdSubCat_Poster
 	{
 		displayName="Posters";
+	};
+	class 89th_EdSubCat_Furniture
+	{
+		displayName="Furniture";
+	};
+	class 89th_EdSubCat_Display
+	{
+		displayName="Display";
+	};
+	class 89th_EdSubCat_Flags
+	{
+		displayName="Flags";
+	};
+};
+class CfgFactionClasses
+{
+	class 89th_JTF_Blufor
+	{
+		dlc="89th JTF";
+		displayName="[89th] JTF";
+		author="89th JTF Mod Team";
+		side=1;
+		icon="\OPTRE_Core\data\icon_UNSC_ca.paa";
+		flag="\OPTRE_Core\Data\flag_UNSC_ca.paa";
+		primaryLanguage="EN";
 	};
 };
---
+++ @89th_Mission_Assets/Addons/89th_JTF_Assets/Display/Config.cpp
@@ -0,0 +1,60 @@
+class CfgPatches
+{
+	class 89th_JTF_Display
+	{
+		author="Flips"
+		units[]=
+		{
+			"89th_EpsilonHelmet_Object"
+		};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_structures_f",
+			"OPTRE_Core",
+			"89th_Armor",
+		};
+	};
+};
+class CfgVehicles
+{	
+	class House;
+	class House_F: House
+	{
+		class DestructionEffects;
+	};
+	class 89th_EpsilonHelmet_Object: House_F
+	{
+		dlc="89thJTF";
+		scope=2;
+		scopeCurator=0;
+		displayName="CH252 Helmet (Epsilon)";
+		icon="\OPTRE_UNSC_Units\Army\icons\Army_Helmet_wdl.paa";
+		model="\OPTRE_UNSC_Units\Army\helmet.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"camo3",
+			"H_Collar",
+			"H_Neck",
+			"H_UNSCVacLower",
+			"H_UNSCVacVisor",
+			"H_VacCollar",
+			"H_Ghillie"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Epsi\89th_marine_helmet_Epsilon_WDL_CO.paa",
+			"\optre_unsc_units\army\data\helmet_visor_ca.paa",
+			"\optre_unsc_units\army\data\ghillie_woodland_co.paa"
+		};
+		author="Tas, Geek, and Vespade";
+		armor=999999;
+		armorStructural=999;
+		editorCategory="89th_EdCat_JTF";
+		editorSubcategory="89th_EdSubCat_Display";
+	};
+
+};

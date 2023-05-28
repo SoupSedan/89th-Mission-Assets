6c1e4467b273461a131e52da08202316e19d41a5
Flipoholic <kreepythegamer@gmail.com>
2023-05-27 22:38:15 -0500

    Reorganization

--- @89th_Mission_Assets/Addons/89th_JTF_Assets/Props/config.cpp
+++ @89th_Mission_Assets/Addons/89th_JTF_Assets/Props/config.cpp
@@ -1,11 +1,12 @@
 class CfgPatches
 {
-	class 89th_JTF_Display
+	class 89th_JTF_Props
 	{
 		author="Flips";
 		units[]=
 		{
-			"89th_EpsilonHelmet_Object"
+			"89th_Breadcouch",
+			"89th_EpsilonHelmet"
 		};
 		weapons[]={};
 		requiredVersion=0.1;
@@ -22,6 +23,28 @@ class CfgVehicles
 	class House_F: House
 	{
 		class DestructionEffects;
+	};
+	class 89th_Breadcouch: House_F
+	{
+		dlc="89thJTF";
+		scope=2;
+		scopeCurator=2;
+		displayName="BreadCouch";
+		icon="\OPTRE_UNSC_Units\Army\icons\Army_Helmet_wdl.paa";
+		model = "\89th_JTF_Assets\Props\breadCouch.p3d";
+		author = "EpsilonS";
+		armor = 999999;
+		armorStructural = 999;
+		editorCategory = "89th_EdCat_JTF";
+		editorSubcategory = "89th_EdSubCat_Props";
+		hiddenSelections[] =
+		{
+			"camo",
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"89th_JTF_Assets\Props\Data\breadCouch_CO.paa",
+		};
 	};
 	class 89th_EpsilonHelmet_Object: House_F
 	{
@@ -53,7 +76,6 @@ class CfgVehicles
 		armor=999999;
 		armorStructural=999;
 		editorCategory="89th_EdCat_JTF";
-		editorSubcategory="89th_EdSubCat_Display";
+		editorSubcategory="89th_EdSubCat_Props";
 	};
-
 };
